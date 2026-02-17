4f8d718a8dd89fa7cd7ec2b88eef89430be369a7
bkoimett <benkoimet@gmail.com>
2026-02-17 21:21:27 +0300

    feat: create basic websocket functionality

--- backend/main.go
+++ backend/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"log"
 	"net/http"
 	
-
-	// "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+        return true // Allow all origins (for development)
+    },
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+    // Upgrade initial GET request to a WebSocket
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer ws.Close()
+
+    for {
+        messageType, p, err := ws.ReadMessage()
+        if err != nil {
+            log.Println("Read error:", err)
+            break
+        }
+        
+        // Echo message back
+        if err := ws.WriteMessage(messageType, p); err != nil {
+            log.Println("Write error:", err)
+            break
+        }
+    }	
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my webpage hosted on %q", html.EscapeString(r.URL.Path))
-	})
-
-	// http.HandleFunc("/ws", handleConnections)
+	http.HandleFunc("/ws", handleConnections)
 
 	fmt.Println("Websocket server started on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil) 
